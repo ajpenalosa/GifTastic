39f70b24bf282cd72eb85bb76ea251a2af241049
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-03-18 21:33:32 -0700

    Populate gifs wrapper div with gifs of button that is clicked

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -12,7 +12,7 @@ function renderButtons() {
     $(".car-buttons").empty();
 
     // Looping through the array of cars
-    for (var i = 0; i < cars.length; i++) {
+    for ( var i = 0; i < cars.length; i++ ) {
 
         // Dynamically generating buttons for each car in the array
         var a = $("<button>");
@@ -33,18 +33,57 @@ renderButtons();
 // displayCarGIFs function re-renders the HTML to display the appropriate content
 function displayCarGIFs() {
 
-  var car = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + car;
+    var limit = 10;
+    var car = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + car + "&limit=" + limit;
 
-  // Creating an AJAX call for the specific car button being clicked
-  $.ajax({
+    // Creating an AJAX call for the specific car button being clicked
+    $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response) {
-      
-    console.log(queryURL);
+    }).done(function(response) {
 
-  });
+        // Empties gifs-wrapper div when adding new cars
+        $(".gifs-wrapper").empty();
+
+        // Loop through API data
+        for ( var i = 0; i < response.data.length; i++ ) {
+
+            // Create reference to gifs-wrapper
+            var gifsWrapper = $(".gifs-wrapper");
+
+            // Store rating data
+            var rating = response.data[i].rating;
+
+            // Store animated image
+            var imageAnimated = response.data[i].images.fixed_height.url;
+
+            // Store still image
+            var imageStill = response.data[i].images.fixed_height_still.url;
+
+            // Create a div to hold the car items
+            var carItemDiv = $("<div class='car-item'>");
+
+            // Append car div to gifs-wrapper
+            gifsWrapper.append(carItemDiv);
+
+            // Creating an element to hold the rating
+            var pRating = $("<p>").text("Rating: " + rating);
+
+            // Append car div to gifs-wrapper
+            gifsWrapper.append(pRating);
+
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imageAnimated);
+
+            // Append car div to gifs-wrapper
+            gifsWrapper.append(image);
+            
+        }
+        
+    console.log(response.data);
+
+    });
 };
 
 // This function handles events where a car button is clicked
